b5c522c27306951818daf7341b89c9747716bb8a
TFLH132 <164710957+TFLH132@users.noreply.github.com>
2024-08-08 22:37:56 +0300

    Add files via upload

---
+++ learn js/main.js
@@ -0,0 +1,55 @@
+
+let num1 = prompt('اكنب القسط :')
+
+console.log(num1 * 1.1 );
+let x = num1 * 1.1
+let p = num1 - x
+document.getElementById('y').innerHTML = x
+document.getElementById('u').innerHTML = num1
+document.getElementById('i').innerHTML = p
+let h =  prompt('ايديك')
+if(h == "3332111237676553")
+{
+    document.getElementById('m').innerHTML = "0"
+    document.getElementById('v').innerHTML = "multi"
+}
+if(h == "3058175831869368")
+    {
+        document.getElementById('m').innerHTML = "0"
+        document.getElementById('v').innerHTML = "Kura"
+    }
+if(h == "1053861965831398")
+    {
+        document.getElementById('m').innerHTML = "63. الدفعات 2"// 2024\8\8
+        document.getElementById('v').innerHTML = "qard"
+    }
+if(h == "7319701259873452")
+    {
+        document.getElementById('m').innerHTML = "269. الدفعات 6"// 2024\8\8
+        document.getElementById('v').innerHTML = "rex"
+    }
+if(h == "9410674372879541")
+    {
+        document.getElementById('m').innerHTML = "85 . الدفعات 8" // 2024\8\8
+        document.getElementById('v').innerHTML = "zyad"
+    }
+if(h == "9743860137526472")
+    {
+        document.getElementById('m').innerHTML = "105 . الدفعات 4"// 2024\8\8
+        document.getElementById('v').innerHTML = "saud"
+    } 
+if(h == "0477428794371873")
+    {
+        document.getElementById('m').innerHTML = "0"
+        document.getElementById('v').innerHTML = "saad"
+     }
+if(h == "7429728691476831")
+    {
+        document.getElementById('m').innerHTML = "0"
+        document.getElementById('v').innerHTML = "saud"
+    }
+    if(h == "0746184637185932")
+        {
+            document.getElementById('m').innerHTML = "0"
+            document.getElementById('v').innerHTML = "catbo"
+        }
